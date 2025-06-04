762f6765a7bde6ae154d773c12921d821fe1063e
mami <malikhan.dev2020@gmail.com>
2025-06-04 07:01:56 -0700

    cleaning up markups

--- Utilities/Ensure.cs
+++ Utilities/Ensure.cs
@@ -5,7 +5,7 @@ namespace FoodAssembly.Utilities
     public static class Ensure
     {
         public static bool NotNull<T>(T? item) where T : class => item != null;
-        public static bool EnumerableNotNull<T>(T? item) where T : IEnumerable => item != null;
+        public static bool NotNull<T>(this IEnumerable<T> item) where T : class => item != null;
 
     }
 }
