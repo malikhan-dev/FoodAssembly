470e5f56586ae4387243ca5c3220f3d8fd6a641b
mami <malikhan.dev2020@gmail.com>
2025-06-05 08:58:54 -0700

    Food Party step 1. components initialized. layouts created. timer sets

---
+++ Models/FoodEventModel.cs
@@ -0,0 +1,28 @@
+﻿namespace FoodAssembly.Models
+{
+    public class FoodEvent
+    {
+        public string EventTitle { get; set; } = "";
+
+        public string EventDescription { get; set; } = "";
+
+        public DateTime EndAt { get; set; } = new();
+
+        public string IconUrl { get; set; } = "";
+
+        public IEnumerable<EventItems> Items { get; set; } = Enumerable.Empty<EventItems>();
+    }
+
+    public class EventItems 
+    {
+        public string FoodName { get; set; } = "";
+        public decimal DeliveryPrice { get; set; }
+        public string VendorName { get; set; } = "";
+        public decimal Price { get; set; } = decimal.Zero;
+        public decimal PriceWithDiscount { get; set; } = decimal.Zero;
+        public int Remaining { get; set; } = 0;
+        public int DiscountPercent { get; set; } = 0;
+        public string ImageUrl { get; set; } = "";
+    }
+
+}
---
+++ Utilities/DateHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+
+namespace FoodAssembly.Utilities
+{
+    public static class DateHelper
+    {
+        public static string CalculateDiff(DateTime TargetDate)
+        {
+            var diff = TargetDate - DateTime.Now;
+
+            return string.Concat(
+                                        diff.Hours.ToString().Length > 1 ? diff.Hours.ToString() : string.Concat("0", diff.Hours.ToString()), ":",
+                                        diff.Minutes.ToString().Length > 1 ? diff.Minutes.ToString() : string.Concat("0", diff.Minutes.ToString()), ":",
+                                        diff.Seconds.ToString().Length > 1 ? diff.Seconds.ToString() : string.Concat("0", diff.Seconds.ToString()));
+
+        }
+    }
+}
